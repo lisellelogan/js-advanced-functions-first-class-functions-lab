032a34602e10038d503ed6473e7f9598de4132e1
lisellelogan <liselle.logan@yahoo.com>
2021-04-13 11:59:25 -0400

    commit tree 8bc48ba014b52e8a19e94ff1e01a9161f636c7d0

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+// returnFirstTwoDrivers function
+//should return first two drivers from passed in array
+const returnFirstTwoDrivers = function(arr){
+   return arr.slice(0,2);
+}
+
+//return last two drivers of given array
+const returnLastTwoDrivers = function(arr){
+    return arr.slice(-2);
+}
+
+//has returnFirstTwoDrivers function as first element
+//has returnLastTwoDrivers function as last element
+//allows us to invoke either function from the array
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//createFareMultiplier returns a function that will multiply a fare for a ride
+//takes in one argument: integer
+
+function createFareMultiplier(num){
+    return function(value){
+        return num * value;
+    }
+}
+
+//fareDoubler is function  that doubles fares
+function fareDoubler(fare){
+    return fare * 2;
+}
+
+//fareDoubler is function  that triples fares
+function fareTripler(fare){
+    return fare * 3;
+}
+
+//selectDifferentDrivers function takes in two arguments: arr of drivers, function
+//return either first two drivers or last two drivers depending on which function is passed
+function selectDifferentDrivers(arrOfDrivers, driverFunction){
+    return driverFunction(arrOfDrivers);
+}
